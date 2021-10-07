66885eac7c9d26391ea66cb99b44ed2c0a36308d
Muhammad Sohail <sohailabbas.100.10@gmail.com>
2021-10-07 20:21:35 +0500

    Complete

---
+++ src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+  {
+    title: 'Gawadar',
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus consequatur esse officiis temporibus illum, voluptatum porro modi iste hic, vel, provident aut adipisci neque quod consequuntur fugiat exercitationem facilis reiciendis?",
+    imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Ormara',
+    description:
+     " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, similique! Corrupti voluptates possimus repellendus unde officiis consequuntur voluptas, dolor consequatur non error doloribus iusto ea aliquam dignissimos qui, voluptatibus quibusdam.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+    time: 1500,
+  },
+];
+
+export default places;

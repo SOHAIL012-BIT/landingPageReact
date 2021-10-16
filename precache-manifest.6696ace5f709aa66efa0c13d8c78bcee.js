daf8daa6400d9c218f84456286df294ca06b9bd7
Muhammad Sohail <sohailabbas.100.10@gmail.com>
2021-10-16 15:41:49 +0500

    Updates

---
+++ precache-manifest.6696ace5f709aa66efa0c13d8c78bcee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a354e10d9760164b782fd87942b60685",
+    "url": "/landingPageReact/index.html"
+  },
+  {
+    "revision": "5bb8158bde29d84059ad",
+    "url": "/landingPageReact/static/js/2.b12b5809.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/landingPageReact/static/js/2.b12b5809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e19341e904868844f4",
+    "url": "/landingPageReact/static/js/main.da281224.chunk.js"
+  },
+  {
+    "revision": "58fe581899b93e7d88b9",
+    "url": "/landingPageReact/static/js/runtime-main.9ee91152.js"
+  }
+]);
